a4f1bf096b8b4113a0efdfa62d059f10434f4413
cc-chichen <chichen@gapp.nthu.edu.tw>
2025-02-14 21:58:55 +0800

    add highlightCup in js

--- _16_project/script.js
+++ _16_project/script.js
@@ -1,1 +1,25 @@
+const smallCup = document.querySelectorAll('.cup-small');
+const remained = document.getElementById('remained');
+const liters = document.getElementById('liters');
+const percentage = document.getElementById('percentage');
 
+smallCup.forEach((cup, idx) => {
+  cup.addEventListener('click', () => highlightCup(idx));
+});
+
+function highlightCup(idx) {
+  if (
+    smallCup[idx].classList.contains('full') &&
+    !smallCup[idx].nextElementSibling.classList.contains('full')
+  ) {
+    idx--;
+  }
+
+  smallCup.forEach((cup, idx2) => {
+    if (idx2 <= idx) {
+      cup.classList.add('full');
+    } else {
+      cup.classList.remove('full');
+    }
+  });
+}
